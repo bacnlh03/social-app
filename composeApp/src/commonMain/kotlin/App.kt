5d450790ba855bc1dd6a96227f9247fdfc55f29d
Bắc Nguyễn <hoaibac1103@gmail.com>
2023-11-25 11:07:17 +0700

    feat: create register screen

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,10 +1,11 @@
 import androidx.compose.runtime.Composable
 import app.login.LoginScreen
+import app.register.components.RegisterScreen
 import app.utils.theme.SocialAppTheme
 
 @Composable
 fun App() {
     SocialAppTheme {
-        LoginScreen()
+        RegisterScreen()
     }
 }
---
+++ composeApp/src/commonMain/kotlin/app/register/RegisterState.kt
@@ -0,0 +1,1 @@
+package app.registerdata class RegisterState(	val email: String = "",	val emailError: String? = null,	val password: String = "",	val passwordError: String? = null,	val confirmPassword: String = "",	val confirmPasswordError: String? = null,	val isLoading: Boolean = false,	val isSuccess: Boolean = false,	val authError: String? = null)
