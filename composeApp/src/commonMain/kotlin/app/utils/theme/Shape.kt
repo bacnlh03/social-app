cb0026c91cd29cd7b8d32bee10d68a468e6810ac
Bắc Nguyễn <hoaibac1103@gmail.com>
2023-11-25 10:42:31 +0700

    feat: create app theme

---
+++ composeApp/src/commonMain/kotlin/app/utils/theme/Shape.kt
@@ -0,0 +1,1 @@
+package app.utils.themeimport androidx.compose.foundation.shape.RoundedCornerShapeimport androidx.compose.material3.Shapesval Shapes = Shapes(	small = RoundedCornerShape(SmallSpacing),	medium = RoundedCornerShape(MediumSpacing),	large = RoundedCornerShape(LargeSpacing))
